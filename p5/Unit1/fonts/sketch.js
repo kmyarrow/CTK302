5f45b38b4af15eaa4157a37f07eeb3e4dfba7012
kmyarrow <59849682+kmyarrow@users.noreply.github.com>
2020-09-04 23:32:40 -0500

    3

--- p5/Unit1/fonts/sketch.js
+++ p5/Unit1/fonts/sketch.js
@@ -2,15 +2,15 @@ var f;
 
 function setup() {
  createCanvas(800,800);
-}
-f = loadFont("assets/k.ttf");
-textAlign(CENTER);
 
+f = loadFont("assets/p.ttf");
+textAlign(CENTER);
+}
 function draw() {
 background(100);
 
 textFont(f);
-textSize(60);
+textSize(100);
 
-text("hello", width/2, height/2);
+text("hello", width/2, height/2, 200, 200);
 }
