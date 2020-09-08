87aad8e6bb15415e14600328a2246ea81502bd30
kmyarrow <59849682+kmyarrow@users.noreply.github.com>
2020-09-08 11:19:35 -0500

    4

---
+++ p5/Unit1/sound/sketch.js
@@ -0,0 +1,23 @@
+var song1 ;
+
+function preload(){
+  song1 = loadSound("assets/wepaid.mp3") ;
+}
+
+function setup() {
+createCanvas(800,800);
+song1.loop() ;
+}
+
+function draw() {
+background(100);
+}
+
+function mouseReleased() {
+    if (song1.isPlaying()){
+      song1.pause();
+    } else{
+      song1.loop();
+    }
+
+}
