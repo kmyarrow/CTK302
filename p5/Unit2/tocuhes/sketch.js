0c37c2dc7fe542a34709f6ece02dfb9ad5f937ad
kmyarrow <59849682+kmyarrow@users.noreply.github.com>
2020-10-02 10:06:48 -0500

    touches

--- p5/Unit2/tocuhes/sketch.js
+++ p5/Unit2/tocuhes/sketch.js
@@ -1,7 +1,15 @@
 let numberOfTouches ;
+var img1, img2, img3;
+let state = 0;
+
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+
+  img1 = loadImage("assets/flowers.jpg");
+  img2 = loadImage("assets/2fingers.png");
+  img3 = loadImage("assets/3fingers.jpg");
+  imageMode(CENTER);
 }
 
 function draw() {
@@ -17,19 +25,28 @@ function draw() {
       case 1:
        text("it's kind of lonely here", 5, 22) ;
       // put a picture here
+      image(img1, width/2, height/2, 200,200);
       break ;
 
       case 2:
       text("two fingers are touching the screen", 5, 22) ;
             // put a picture here
+          image(img2, width/2, height/2, 200,200);
       break ;
 
       case 3:
      text("three are touching the screen", 5, 22) ;
             // put a picture here
+            image(img3, width/2, height/2, 200,200);
       break ;
 
 
   }
+}
 
+function mouseReleased(){
+  state++;
+  if(state > 3){
+    state = 0;
+  }
 }
