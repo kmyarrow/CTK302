6a30f1cba58106e57a348c9381317845a98e0467
kmyarrow <59849682+kmyarrow@users.noreply.github.com>
2020-12-08 21:29:52 -0600

    lastone

--- p5/Unit4/gryoscope/sketch.js
+++ p5/Unit4/gryoscope/sketch.js
@@ -44,27 +44,27 @@ function draw() {
 
 
   // Text commands that display debugging data
-  textAlign(LEFT);
-  textSize(20);
+  //textAlign(LEFT);
+  //textSize(20);
   fill('black');
-  text("orientation data:", 25, 25);
-  textSize(15);
-  text("alpha: " + alpha, 25, 50);
-  text("beta: " + beta, 25, 70);
-  text("gamma: " + gamma, 25, 90);
-  textSize(20);
-  text("acceleration data:", 25, 125);
-  textSize(15);
-  text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
-  text("y = " + y.toFixed(2), 25, 170);
-  text("z = " + z.toFixed(4), 25, 190);
+  //text("orientation data:", 25, 25);
+//  textSize(15);
+  //text("alpha: " + alpha, 25, 50);
+  //text("beta: " + beta, 25, 70);
+  //text("gamma: " + gamma, 25, 90);
+  //textSize(20);
+  //text("acceleration data:", 25, 125);
+  //textSize(15);
+  //text("x = " + x.toFixed(2), 25, 150); // //.toFixed means just show (x) decimal places
+  //text("y = " + y.toFixed(2), 25, 170);
+  //text("z = " + z.toFixed(4), 25, 190);
 
   // Text that makes CTK type in the background
   fill('white');
   noStroke();
   textSize(50);
   textAlign(CENTER);
-  text("shoot for the stars", width / 2, height / 2);
+  text("aim for the moon", width / 2, height / 2);
 
 }
 
